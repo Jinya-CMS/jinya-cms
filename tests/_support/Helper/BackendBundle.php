143cb139bea06a755559a82621a6a59c465c655f
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2017-10-25 20:33:22 +0200

    Added ignoring of deprecations

--- tests/_support/Helper/BackendBundle.php
+++ tests/_support/Helper/BackendBundle.php
@@ -5,6 +5,12 @@ namespace Helper;
 // here you can define custom actions
 // all public methods declared in helper class will be available in $I
 
+use Codeception\TestInterface;
+
 class BackendBundle extends \Codeception\Module
 {
+    public function _before(TestInterface $test)
+    {
+        parent::_before($test);
+    }
 }
