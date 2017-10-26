0c2bffe3f870f9627a992a48a81b09de22dca482
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2017-10-26 20:18:40 +0000

    Apply fixes from StyleCI

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -39,16 +39,16 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        return dirname(__DIR__).'/var/logs';
+        return dirname(__DIR__) . '/var/logs';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
