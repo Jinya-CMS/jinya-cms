04e516444e0570499a9f0b1ec5ef6b71f5afaf28
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2017-11-07 18:39:40 +0100

    Added bundle for data access

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new BackendBundle\BackendBundle(),
             new HelperBundle\HelperBundle(),
+            new DataBundle\DataBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
