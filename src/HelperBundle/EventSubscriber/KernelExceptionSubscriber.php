697d4d548bbe2457d539059c3a25ad1b01ac6f7a
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-01-15 22:42:13 +0100

    Added redirect for any exception

---
+++ src/HelperBundle/EventSubscriber/KernelExceptionSubscriber.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imanu
+ * Date: 15.01.2018
+ * Time: 22:32
+ */
+
+namespace HelperBundle\EventSubscriber;
+
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\Routing\Router;
+
+class KernelExceptionSubscriber implements EventSubscriberInterface
+{
+    /** @var Router */
+    private $router;
+
+    /**
+     * KernelExceptionSubscriber constructor.
+     * @param Router $router
+     */
+    public function __construct(Router $router)
+    {
+        $this->router = $router;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            'kernel.exception' => 'onException'
+        ];
+    }
+
+    public function onException(GetResponseForExceptionEvent $event)
+    {
+        $request = $event->getRequest();
+
+        $route = $this->router->getRouteCollection()->get($request->get('_route'));
+        if ($route->getPath() === '/{route}') {
+            $event->setResponse(new RedirectResponse('/'));
+        }
+    }
+}
