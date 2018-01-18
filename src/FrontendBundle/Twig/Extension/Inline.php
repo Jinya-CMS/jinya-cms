eeb90dcb86c8ee605a8806aff775df4462e893d9
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-01-18 22:59:13 +0100

    Added share buttons

---
+++ src/FrontendBundle/Twig/Extension/Inline.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FrontendBundle\Twig\Extension;
+
+use function file_get_contents;
+
+class Inline extends \Twig_Extension
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return 'inline';
+    }
+
+    public function getFunctions()
+    {
+        return [
+            'inline' => new \Twig_Function('inline', [$this, 'inlineFile'])
+        ];
+    }
+
+    public function inlineFile(string $filename): string
+    {
+        return file_get_contents($filename);
+    }
+}
