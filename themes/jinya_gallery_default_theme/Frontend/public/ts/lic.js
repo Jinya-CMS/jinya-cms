a2653e31696e61c86de6f172c934ba120015eaf4
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-01-26 19:29:55 +0100

    Finished form design for now

---
+++ themes/jinya_gallery_default_theme/Frontend/public/ts/lic.js
@@ -0,0 +1,15 @@
+/**
+ * selectFx.js v1.0.0
+ * http://www.codrops.com
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2014, Codrops
+ * http://www.codrops.com
+ */
+/*!
+ * perfect-scrollbar v1.3.0
+ * (c) 2017 Hyunje Jun
+ * @license MIT
+ */
