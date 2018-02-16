b2ad2c413787b41570d57d4ffd376903a8fb08f8
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-02-16 17:21:27 +0100

    Updated symfony

--- config/bundles.php
+++ config/bundles.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: imanu
- * Date: 16.02.2018
- * Time: 15:17
- */
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
