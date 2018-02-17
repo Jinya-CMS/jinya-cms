856ba8ae7b82a083c59e4113b958d9f2f43b5695
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-02-17 17:25:47 +0100

    Added minification

--- themes/jinya_gallery_default_theme/public/Gulpfile.js
+++ themes/jinya_gallery_default_theme/public/Gulpfile.js
@@ -7,47 +7,43 @@ const rename = require('gulp-rename');
 
 gulp.task('build:designer:babel', () => {
     gulp.src([
-        'js/util/**/*.js',
-        'js/designer/**/*.js'
+        'src/js/util/**/*.js',
+        'src/js/designer/**/*.js'
     ])
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['babel-preset-env']
         }))
         .pipe(concat('designer.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('js'));
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'))
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(rename('designer.min.js'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build:frontend:babel', () => {
     gulp.src([
-        'js/util/**/*.js',
-        'js/frontend/perfect-scrollbar.js',
-        'js/frontend/scrollhelper.js'
+        'src/js/util/**/*.js',
+        'src/js/frontend/perfect-scrollbar.js',
+        'src/js/frontend/scrollhelper.js'
     ])
         .pipe(sourcemaps.init())
         .pipe(babel({
             presets: ['babel-preset-env']
         }))
         .pipe(concat('frontend.js'))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('build:frontend:uglify', ['build:frontend:babel'], () => {
-    gulp.src('js/frontend.js')
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'))
+        .pipe(sourcemaps.init())
         .pipe(uglify())
         .pipe(rename('frontend.min.js'))
-        .pipe(gulp.dest('js'));
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build:designer:uglify', ['build:designer:babel'], () => {
-    gulp.src('js/designer.js')
-        .pipe(uglify())
-        .pipe(rename('designer.min.js'))
-        .pipe(gulp.dest('js'));
-});
-
-gulp.task('build', ['build:designer:uglify', "build:frontend:uglify"]);
+gulp.task('build', ['build:designer:babel', "build:frontend:babel"]);
 
 gulp.task('default', ['build']);
