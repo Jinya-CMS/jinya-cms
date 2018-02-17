93297e0a1701c7cda58194a2f8846647f8a01eff
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-02-17 17:10:26 +0100

    Moved utility classes

---
+++ themes/jinya_gallery_default_theme/public/js/util/i18n.js
@@ -0,0 +1,9 @@
+class i18n {
+    /**
+     * Gets the text for the specified identifier
+     * @param identifier string
+     */
+    getText = (identifier) => {
+        return texts[identifier];
+    };
+}
