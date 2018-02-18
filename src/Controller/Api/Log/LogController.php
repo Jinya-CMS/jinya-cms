d167b1b13e04dff6217e2654ccfefa5810d1dec3
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-02-18 23:08:54 +0100

    Finished log controller

--- src/Controller/Api/Log/LogController.php
+++ src/Controller/Api/Log/LogController.php
@@ -10,6 +10,9 @@ namespace Jinya\Controller\Api\Log;
 
 use Jinya\Framework\BaseApiController;
 use Jinya\Services\Log\LogServiceInterface;
+use Monolog\Logger;
+use Psr\Log\LoggerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,6 +22,7 @@ class LogController extends BaseApiController
 {
     /**
      * @Route("/api/log", methods={"GET"}, name="api_log_get_all")
+     * @IsGranted("ROLE_ADMIN", statusCode=403)
      *
      * @param Request $request
      * @param LogServiceInterface $logService
@@ -48,6 +52,61 @@ class LogController extends BaseApiController
 
             return $this->formatListResult($entityCount, $offset, $count, $parameter, 'api_log_get_all', $entities);
         });
+
+        return $this->json($data, $status);
+    }
+
+    /**
+     * @Route("/api/log/{id}", methods={"GET"}, name="api_log_get")
+     * @IsGranted("ROLE_ADMIN", statusCode=403)
+     *
+     * @param int $id
+     * @param LogServiceInterface $logService
+     * @return Response
+     */
+    public function getAction(int $id, LogServiceInterface $logService): Response
+    {
+        list($data, $status) = $this->tryExecute(function () use ($id, $logService) {
+            return $logService->get($id);
+        });
+
+        return $this->json($data, $status);
+    }
+
+    /**
+     * @Route("/api/log", methods={"POST"}, name="api_log_post")
+     * @IsGranted("IS_AUTHENTICATED_ANONYMOUSLY")
+     *
+     * @param Request $request
+     * @param LoggerInterface $logger
+     * @return Response
+     */
+    public function postAction(Request $request, LoggerInterface $logger): Response
+    {
+        list($data, $status) = $this->tryExecute(function () use ($request, $logger) {
+            $level = Logger::toMonologLevel($this->getValue('level', 'INFO'));
+            $message = $this->getValue('message', null);
+            $context = $request->attributes->all();
+            $context['referer'] = $request->server->get('HTTP_REFERER');
+
+            $logger->log($level, $message, $context);
+        }, Response::HTTP_NO_CONTENT);
+
+        return $this->json($data, $status);
+    }
+
+    /**
+     * @Route("/api/log", methods={"DELETE"}, name="api_log_delete")
+     * @IsGranted("ROLE_SUPER_ADMIN", statusCode=403)
+     *
+     * @param LogServiceInterface $logService
+     * @return Response
+     */
+    public function deleteAction(LogServiceInterface $logService): Response
+    {
+        list($data, $status) = $this->tryExecute(function () use ($logService) {
+            $logService->clear();
+        }, Response::HTTP_NO_CONTENT);
 
         return $this->json($data, $status);
     }
