c3d410b11da9eff20a993ee95e6a3aa8a66f56cd
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-03-05 22:02:07 +0100

    Added configuration controller

---
+++ src/Controller/Api/Configuration/ConfigurationController.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imanu
+ * Date: 05.03.2018
+ * Time: 21:04
+ */
+
+namespace Jinya\Controller\Api\Configuration;
+
+
+use Jinya\Formatter\Configuration\ConfigurationFormatterInterface;
+use Jinya\Framework\BaseApiController;
+use Jinya\Services\Configuration\ConfigurationServiceInterface;
+use Jinya\Services\Theme\ThemeServiceInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConfigurationController extends BaseApiController
+{
+    /**
+     * @Route("/api/configuration", methods={"GET"}, name="api_configuration_get")
+     * @IsGranted("ROLE_WRITER")
+     *
+     * @param ConfigurationServiceInterface $configurationService
+     * @param ConfigurationFormatterInterface $configurationFormatter
+     * @return Response
+     */
+    public function getAction(ConfigurationServiceInterface $configurationService, ConfigurationFormatterInterface $configurationFormatter): Response
+    {
+        list($data, $status) = $this->tryExecute(function () use ($configurationService, $configurationFormatter) {
+            return $configurationFormatter
+                ->init($configurationService->getConfig())
+                ->designerTheme()
+                ->frontendTheme()
+                ->format();
+        });
+
+        return $this->json($data, $status);
+    }
+
+    /**
+     * @Route("/api/configuration/frontend/{themeName}", methods={"PUT"}, name="api_configuration_frontend_put")
+     * @IsGranted("ROLE_WRITER")
+     *
+     * @param string $themeName
+     * @param ConfigurationServiceInterface $configurationService
+     * @param ThemeServiceInterface $themeService
+     * @return Response
+     */
+    public function putFrontendAction(string $themeName, ConfigurationServiceInterface $configurationService, ThemeServiceInterface $themeService): Response
+    {
+        list($data, $status) = $this->tryExecute(function () use ($themeName, $configurationService, $themeService) {
+            $configuration = $configurationService->getConfig();
+            $configuration->setCurrentFrontendTheme($themeService->getTheme($themeName));
+
+            $configurationService->writeConfig($configuration);
+        }, Response::HTTP_NO_CONTENT);
+
+        return $this->json($data, $status);
+    }
+
+    /**
+     * @Route("/api/configuration/designer/{themeName}", methods={"PUT"}, name="api_configuration_designer_put")
+     * @IsGranted("ROLE_WRITER")
+     *
+     * @param string $themeName
+     * @param ConfigurationServiceInterface $configurationService
+     * @param ThemeServiceInterface $themeService
+     * @return Response
+     */
+    public function putDesignerAction(string $themeName, ConfigurationServiceInterface $configurationService, ThemeServiceInterface $themeService): Response
+    {
+        list($data, $status) = $this->tryExecute(function () use ($themeName, $configurationService, $themeService) {
+            $configuration = $configurationService->getConfig();
+            $configuration->setCurrentDesignerTheme($themeService->getTheme($themeName));
+
+            $configurationService->writeConfig($configuration);
+        }, Response::HTTP_NO_CONTENT);
+
+        return $this->json($data, $status);
+    }
+}
---
+++ src/Formatter/Configuration/ConfigurationFormatter.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imanu
+ * Date: 05.03.2018
+ * Time: 21:54
+ */
+
+namespace Jinya\Formatter\Configuration;
+
+
+use Jinya\Entity\Configuration;
+use Jinya\Formatter\Theme\ThemeFormatterInterface;
+
+class ConfigurationFormatter implements ConfigurationFormatterInterface
+{
+    /** @var array */
+    private $formattedData;
+    /** @var Configuration */
+    private $configuration;
+
+    /** @var ThemeFormatterInterface */
+    private $themeFormatter;
+
+    /**
+     * ConfigurationFormatter constructor.
+     * @param ThemeFormatterInterface $themeFormatter
+     */
+    public function __construct(ThemeFormatterInterface $themeFormatter)
+    {
+        $this->themeFormatter = $themeFormatter;
+    }
+
+    /**
+     * Initializes the formatter
+     *
+     * @param Configuration $configuration
+     * @return ConfigurationFormatterInterface
+     */
+    public function init(Configuration $configuration): ConfigurationFormatterInterface
+    {
+        $this->configuration = $configuration;
+
+        return $this;
+    }
+
+    /**
+     * Formats the designer theme
+     *
+     * @return ConfigurationFormatterInterface
+     */
+    public function designerTheme(): ConfigurationFormatterInterface
+    {
+        $this->formattedData['designer']['theme'] = $this->themeFormatter
+            ->init($this->configuration->getCurrentDesignerTheme())
+            ->name()
+            ->displayName()
+            ->description()
+            ->format();
+
+        return $this;
+    }
+
+    /**
+     * Formats the frontend theme
+     *
+     * @return ConfigurationFormatterInterface
+     */
+    public function frontendTheme(): ConfigurationFormatterInterface
+    {
+        $this->formattedData['frontend']['theme'] = $this->themeFormatter
+            ->init($this->configuration->getCurrentFrontendTheme())
+            ->name()
+            ->displayName()
+            ->description()
+            ->format();
+
+        return $this;
+    }
+
+    /**
+     * Formats the content of the @see FormatterInterface into an array
+     *
+     * @return array
+     */
+    public function format(): array
+    {
+        return $this->formattedData;
+    }
+}
