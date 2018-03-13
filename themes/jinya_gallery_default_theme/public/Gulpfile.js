69109c893cd4dd186956bcf2d0a6395e5bf37271
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-03-13 18:38:58 +0100

    Adjusted theme for designer

--- themes/jinya_gallery_default_theme/public/Gulpfile.js
+++ themes/jinya_gallery_default_theme/public/Gulpfile.js
@@ -5,25 +5,6 @@ const concat = require('gulp-concat');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 
-gulp.task('build:designer:babel', () => {
-    gulp.src([
-        'src/js/util/**/*.js',
-        'src/js/designer/**/*.js'
-    ])
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            presets: ['babel-preset-env']
-        }))
-        .pipe(concat('designer.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist'))
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(rename('designer.min.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist'));
-});
-
 gulp.task('build:frontend:babel', () => {
     gulp.src([
         'src/js/util/**/*.js',
@@ -44,6 +25,6 @@ gulp.task('build:frontend:babel', () => {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('build', ['build:designer:babel', "build:frontend:babel"]);
+gulp.task('build', ["build:frontend:babel"]);
 
 gulp.task('default', ['build']);
