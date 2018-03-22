3b6eb289290a2e68f762f4225055b78447970c64
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-03-22 22:09:48 +0100

    Added card buttons

---
+++ themes/jinya_gallery_default_theme/Designer/src/components/Framework/Markup/Listing/Card/CardButton.vue
@@ -0,0 +1,36 @@
+<template>
+    <button class="jinya-card-button" :class="`jinya-card-button--${type}`" @click="$emit('click')">
+        <span v-if="icon" class="mdi" :class="`mdi-${icon}`"></span>
+        <span v-if="text">{{text|jmessage}}</span>
+    </button>
+</template>
+
+<script>
+  export default {
+    name: "jinya-card-button",
+    props: {
+      type: {
+        type: String,
+        validate(input) {
+          return ['details', 'edit', 'delete'].includes(input.toString().toLowerCase());
+        }
+      },
+      icon: {
+        type: String,
+        validate(input) {
+          return this.text || input;
+        }
+      },
+      text: {
+        type: String,
+        validate(input) {
+          return this.icon || input;
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+    @include card-footer-button('jinya-card-button');
+</style>
