c3779e28e883c7b3fe1496d5e662c43b59edd61f
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-03-28 12:45:52 +0200

    Fixed a bug in the button component

--- themes/jinya_gallery_default_theme/Designer/src/components/Framework/Markup/Button.vue
+++ themes/jinya_gallery_default_theme/Designer/src/components/Framework/Markup/Button.vue
@@ -1,9 +1,9 @@
 <template>
-    <a v-if="href" :href="href" v-jinya-message="label" class="jinya-button" :class="additionalClasses"></a>
+    <a v-if="href" :href="href" v-jinya-message="label" class="jinya-button" :class="getAdditionalClasses()"></a>
     <router-link v-else-if="to" :to="routeTarget" v-jinya-message="label" class="jinya-button"
-                 :class="additionalClasses"/>
+                 :class="getAdditionalClasses()"/>
     <button :type="type" v-else @click="$event => $emit('click', $event)" v-jinya-message="label" class="jinya-button"
-            :disabled="isDisabled" :class="additionalClasses"></button>
+            :disabled="isDisabled" :class="getAdditionalClasses()"></button>
 </template>
 
 <script>
@@ -34,24 +34,27 @@
         }
       }
     },
-    data() {
-      const data = {
-        routeTarget: this.to ? {
+    methods: {
+      getAdditionalClasses() {
+        return {
+          'is--primary': this.isDisabled ? false : this.isPrimary,
+          'is--secondary': this.isDisabled ? false : this.isSecondary,
+          'is--danger': this.isDisabled ? false : this.isDanger,
+          'is--success': this.isDisabled ? false : this.isSuccess,
+          'is--default': this.isDisabled ? false : !(this.isSuccess || this.isDanger || this.isPrimary || this.isSecondary),
+          'is--inverse': this.isInverse,
+          'is--disabled': this.isDisabled
+        };
+      }
+    },
+    computed: {
+      routeTarget() {
+        return this.to ? {
           name: ObjectUtils.valueByKeypath(Routes, this.to).name,
           params: this.params,
           query: this.query
-        } : undefined
-      };
-      data.additionalClasses = {
-        'is--primary': this.isDisabled ? false : this.isPrimary,
-        'is--secondary': this.isDisabled ? false : this.isSecondary,
-        'is--danger': this.isDisabled ? false : this.isDanger,
-        'is--success': this.isDisabled ? false : this.isSuccess,
-        'is--default': this.isDisabled ? false : !(this.isSuccess || this.isDanger || this.isPrimary || this.isSecondary),
-        'is--inverse': this.isInverse,
-        'is--disabled': this.isDisabled
-      };
-      return data;
+        } : undefined;
+      }
     }
   }
 </script>
