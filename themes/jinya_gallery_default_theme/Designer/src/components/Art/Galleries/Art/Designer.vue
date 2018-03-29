1dd69b9a50c307c9750886a0905044c7c784c5ac
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-03-29 14:50:57 +0200

    Added styling for add buttons

--- themes/jinya_gallery_default_theme/Designer/src/components/Art/Galleries/Art/Designer.vue
+++ themes/jinya_gallery_default_theme/Designer/src/components/Art/Galleries/Art/Designer.vue
@@ -1,21 +1,20 @@
 <template>
     <div class="jinya-gallery-designer" :class="`is--${gallery.orientation}`">
         <jinya-loader :loading="loading"/>
-        <div class="jinya-gallery-designer__button" v-if="!loading">
-            <i class="mdi mdi-plus"></i>
-        </div>
-        <template v-if="!loading">
+        <div class="jinya-gallery-designer__button jinya-gallery-designer__button--add" v-if="!loading"></div>
+        <template v-if="!loading" v-for="position in artworks">
             <div class="jinya-gallery-designer__item">
-                <figure class="jinya-gallery-designer__content" v-for="artwork in artworks">
-                    <img :src="artwork.picture" class="jinya-gallery-designer__image">
-                    <figcaption class="jinya-gallery-designer__image-name">{{gallery.name}}</figcaption>
-                </figure>
-                <div class="jinya-gallery-designer__button-move--left"></div>
-                <div class="jinya-gallery-designer__button-move--right"></div>
+                <div class="jinya-gallery-designer__content">
+                    <img :src="position.artwork.picture" class="jinya-gallery-designer__image">
+                    <div class="jinya-gallery-designer__button jinya-gallery-designer__button--edit"></div>
+                </div>
+                <div class="jinya-gallery-designer__action-bar">
+                    <div class="jinya-gallery-designer__button-move--left"></div>
+                    <span class="jinya-gallery-designer__image-name">{{position.artwork.name}}</span>
+                    <div class="jinya-gallery-designer__button-move--right"></div>
+                </div>
             </div>
-            <div class="jinya-gallery-designer__button">
-                <i class="mdi mdi-plus"></i>
-            </div>
+            <div class="jinya-gallery-designer__button jinya-gallery-designer__button--add"></div>
         </template>
     </div>
 </template>
@@ -23,38 +22,31 @@
 <script>
   import JinyaRequest from "@/components/Framework/Ajax/JinyaRequest";
   import JinyaLoader from "@/components/Framework/Markup/Loader";
+  import DOMUtils from "@/components/Framework/Utils/DOMUtils";
+  import Translator from "@/components/Framework/i18n/Translator";
 
   export default {
     components: {JinyaLoader},
     name: "designer",
-    props: {
-      gallery: {
-        type: Object,
-        default() {
-          return {
-            orientation: 'horizontal',
-            name: ''
-          }
-        }
-      },
-      artworks: {
-        type: Array,
-        default() {
-          return [];
-        }
-      }
-    },
     async mounted() {
       this.loading = true;
       try {
+        const gallery = await JinyaRequest.get(`/api/gallery/${this.$route.params.slug}`);
+        this.gallery = gallery.item;
         this.artworks = await JinyaRequest.get(`/api/gallery/${this.$route.params.slug}/artwork`);
+        DOMUtils.changeTitle(Translator.message('art.galleries.designer.title', this.gallery));
       } catch (error) {
       }
       this.loading = false;
     },
     data() {
       return {
-        artworks: gallery.artworks,
+        gallery: {
+          name: '',
+          orientation: '',
+          background: ''
+        },
+        artworks: [],
         loading: false
       };
     }
@@ -62,5 +54,66 @@
 </script>
 
 <style scoped lang="scss">
+    .jinya-gallery-designer {
+        display: flex;
+        height: 100%;
+        width: 100%;
+
+        &.is--horizontal {
+            flex-direction: row;
+            flex-wrap: nowrap;
+            padding-bottom: 10em;
+
+            .jinya-gallery-designer__button {
+                &.jinya-gallery-designer__button--add {
+                    flex-basis: 10em;
+                    width: 10em;
+                }
+            }
+        }
+        &.is--vertical {
+            flex-direction: column;
+
+            .jinya-gallery-designer__button {
+                &.jinya-gallery-designer__button--add {
+                    flex-basis: 10em;
+                    height: 10em;
+                    width: 100%;
+                }
+            }
+        }
+
+        .jinya-gallery-designer__button {
+            flex: 0 0 auto;
+            height: 100%;
+            width: 100%;
+            background: $gray-200;
+            margin: 1em;
+            position: relative;
+            cursor: pointer;
+            transition: transform 0.3s;
+            transform: scale(1, 1);
+            font-weight: bold;
+
+            &:hover {
+                transform: scale(0.9, 0.9);
+                background: $gray-300;
+            }
+
+            &.jinya-gallery-designer__button--add {
+                &::before {
+                    //noinspection CssNoGenericFontName
+                    font-family: "Material Design Icons";
+                    content: "\f415";
+                    font-size: 3em;
+                    color: $primary;
+                    top: 50%;
+                    position: absolute;
+                    left: 50%;
+                    transform: translate(-50%, -50%);
+                }
+            }
+        }
+    }
 
 </style>
