aa0aed131cbd575a6357a702e86bdc14017199ee
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-03-30 22:53:08 +0200

    Fixed translator

--- themes/jinya_gallery_default_theme/Designer/src/components/Framework/Utils/ObjectUtils.js
+++ themes/jinya_gallery_default_theme/Designer/src/components/Framework/Utils/ObjectUtils.js
@@ -1,11 +1,11 @@
 export default {
   valueByKeypath: (target, key) => {
-    return key.split('.').reduce((previous, current) => {
+    return key ? key.split('.').reduce((previous, current) => {
       if (previous) {
         return previous[current];
       } else {
         return current;
       }
-    }, target) || key;
+    }, target) || key : '';
   }
 }
