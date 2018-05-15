ab6c678cab44bfd58f1128bf4a5109d35177f30a
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-05-15 18:41:57 +0200

    Added server side for features

---
+++ src/Controller/Api/Support/FeatureController.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imanuel
+ * Date: 15.05.18
+ * Time: 17:40
+ */
+
+namespace Jinya\Controller\Api\Support;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use Jinya\Framework\BaseApiController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FeatureController extends BaseApiController
+{
+    /** @var string */
+    private $jinyaVersion;
+
+    /**
+     * @Route("/api/support/feature", methods={"POST"}, name="api_support_feature")
+     *
+     * @param Request $request
+     * @param Client $client
+     * @return Response
+     */
+    public function submitAction(Request $request, Client $client): Response
+    {
+        list($data, $status) = $this->tryExecute(function () use ($request, $client) {
+            $feature = [
+                'title' => $this->getValue('title'),
+                'details' => $this->getValue('details'),
+                'who' => $this->getValue('who'),
+            ];
+
+            $response = $client->request('POST', 'https://api.jinya.de/tracker/feature', [
+                RequestOptions::JSON => $feature,
+                RequestOptions::HEADERS => [
+                    'Content-Type' => 'application/json'
+                ]
+            ]);
+
+            return json_decode($response->getBody()->getContents(), true);
+        });
+
+        return $this->json($data, $status);
+    }
+
+    /**
+     * @param string $jinyaVersion
+     */
+    public function setJinyaVersion(string $jinyaVersion): void
+    {
+        $this->jinyaVersion = $jinyaVersion;
+    }
+}
