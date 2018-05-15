97b1a86958a0ccc7910491495a8f545a03bc1aab
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-05-15 18:38:37 +0200

    Added server side for bugs

---
+++ src/Controller/Api/Support/BugController.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imanuel
+ * Date: 15.05.18
+ * Time: 17:40
+ */
+
+namespace Jinya\Controller\Api\Support;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use Jinya\Framework\BaseApiController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BugController extends BaseApiController
+{
+    /** @var string */
+    private $jinyaVersion;
+
+    /**
+     * @Route("/api/support/bug", methods={"POST"}, name="api_support_bug")
+     *
+     * @param Request $request
+     * @param Client $client
+     * @return Response
+     */
+    public function submitAction(Request $request, Client $client): Response
+    {
+        list($data, $status) = $this->tryExecute(function () use ($request, $client) {
+            ob_start();
+            phpinfo();
+            $phpInfo = ob_get_contents();
+            ob_clean();
+
+            $bug = [
+                'phpInfo' => $phpInfo,
+                'title' => $this->getValue('title'),
+                'details' => $this->getValue('details'),
+                'reproduce' => $this->getValue('reproduce'),
+                'severity' => $this->getValue('severity'),
+                'who' => $this->getValue('who'),
+                'url' => $request->headers->has('referer') ? $request->headers->get('referer') : '',
+                'jinyaVersion' => $this->jinyaVersion
+            ];
+
+            $response = $client->request('POST', 'https://api.jinya.de/tracker/bug', [
+                RequestOptions::JSON => $bug,
+                RequestOptions::HEADERS => [
+                    'Content-Type' => 'application/json'
+                ]
+            ]);
+
+            return json_decode($response->getBody()->getContents(), true);
+        });
+
+        return $this->json($data, $status);
+    }
+
+    /**
+     * @param string $jinyaVersion
+     */
+    public function setJinyaVersion(string $jinyaVersion): void
+    {
+        $this->jinyaVersion = $jinyaVersion;
+    }
+}
