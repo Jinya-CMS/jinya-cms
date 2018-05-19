b30242b3f7bf95393ab0bb0fcf773cecece0cae5
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-05-19 14:32:51 +0200

    Added basic update controller

---
+++ src/Controller/UpdateController.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imanuel
+ * Date: 18.05.18
+ * Time: 23:23
+ */
+
+namespace Jinya\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class UpdateController extends AbstractController
+{
+    public function indexAction()
+    {
+
+    }
+
+    public function updateDatabaseAction()
+    {
+
+    }
+
+    public function doneAction()
+    {
+
+    }
+}
