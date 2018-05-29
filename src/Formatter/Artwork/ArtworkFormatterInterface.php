e8bbfb3ceff03f99af75811188a7816487f7f7ef
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-05-29 17:38:40 +0000

    Apply fixes from StyleCI

--- src/Formatter/Artwork/ArtworkFormatterInterface.php
+++ src/Formatter/Artwork/ArtworkFormatterInterface.php
@@ -13,82 +13,81 @@ use Jinya\Formatter\FormatterInterface;
 
 interface ArtworkFormatterInterface extends FormatterInterface
 {
-
     /**
      * Initializes the formatting
      *
      * @param Artwork $artwork
      * @return ArtworkFormatterInterface
      */
-    public function init(Artwork $artwork): ArtworkFormatterInterface;
+    public function init(Artwork $artwork): self;
 
     /**
      * Formats the slug
      *
      * @return ArtworkFormatterInterface
      */
-    public function slug(): ArtworkFormatterInterface;
+    public function slug(): self;
 
     /**
      * Formats the name
      *
      * @return ArtworkFormatterInterface
      */
-    public function name(): ArtworkFormatterInterface;
+    public function name(): self;
 
     /**
      * Formats the description
      *
      * @return ArtworkFormatterInterface
      */
-    public function description(): ArtworkFormatterInterface;
+    public function description(): self;
 
     /**
      * Formats the created info
      *
      * @return ArtworkFormatterInterface
      */
-    public function created(): ArtworkFormatterInterface;
+    public function created(): self;
 
     /**
      * Formats the updated info
      *
      * @return ArtworkFormatterInterface
      */
-    public function updated(): ArtworkFormatterInterface;
+    public function updated(): self;
 
     /**
      * Formats the history
      *
      * @return ArtworkFormatterInterface
      */
-    public function history(): ArtworkFormatterInterface;
+    public function history(): self;
 
     /**
      * Formats the picture
      *
      * @return ArtworkFormatterInterface
      */
-    public function picture(): ArtworkFormatterInterface;
+    public function picture(): self;
 
     /**
      * Formats the labels
      *
      * @return ArtworkFormatterInterface
      */
-    public function labels(): ArtworkFormatterInterface;
+    public function labels(): self;
 
     /**
      * Formats the galleries
      *
      * @return ArtworkFormatterInterface
      */
-    public function galleries(): ArtworkFormatterInterface;
+    public function galleries(): self;
 
     /**
      * Formats the id
      *
      * @return ArtworkFormatterInterface
      */
-    public function id(): ArtworkFormatterInterface;
+    public function id(): self;
 }
