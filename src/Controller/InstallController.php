338214db41d1c5afa8b5186121e9cb10f1a08898
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2018-06-03 13:52:05 +0000

    Apply fixes from StyleCI

--- src/Controller/InstallController.php
+++ src/Controller/InstallController.php
@@ -18,20 +18,24 @@ use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class InstallController
- * @package Jinya\Controller
  */
 class InstallController extends AbstractController
 {
     /** @var SchemaToolInterface */
     private $schemaTool;
+
     /** @var string */
     private $kernelProjectDir;
+
     /** @var UserServiceInterface */
     private $userService;
+
     /** @var \Twig_Environment */
     private $twig;
+
     /** @var MediaServiceInterface */
     private $mediaService;
+
     /** @var ThemeSyncServiceInterface */
     private $themeSyncService;
 
@@ -75,7 +79,7 @@ class InstallController extends AbstractController
                 'appSecret' => uniqid(),
                 'appEnv' => 'prod',
                 'mailerUrl' => $mailerUrl,
-                'mailerSender' => $formData->getMailerSender()
+                'mailerSender' => $formData->getMailerSender(),
             ];
 
             $this->writeEnv($parameters);
@@ -84,7 +88,7 @@ class InstallController extends AbstractController
         }
 
         return $this->render('@Jinya\Installer\Default\index.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
         ]);
     }
 
@@ -110,6 +114,7 @@ class InstallController extends AbstractController
     {
         if ($request->isMethod('POST')) {
             $this->schemaTool->createSchema();
+
             return $this->redirectToRoute('install_admin');
         }
 
@@ -160,7 +165,7 @@ class InstallController extends AbstractController
         }
 
         return $this->render('@Jinya\Installer\Default\createAdmin.html.twig', [
-            'form' => $form->createView()
+            'form' => $form->createView(),
         ]);
     }
 }
