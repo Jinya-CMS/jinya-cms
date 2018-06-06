22b8439bf21c06e1ceabc09df6a4dda223c470e3
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-06-06 21:08:55 +0200

    Adjusted migrations

--- src/Migrations/Version20180606165625.php
+++ src/Migrations/Version20180606165625.php
@@ -18,8 +18,6 @@ final class Version20180606165625 extends AbstractMigration
         $this->addSql('CREATE TABLE video (id INT AUTO_INCREMENT NOT NULL, creator_id INT DEFAULT NULL, updated_by_id INT DEFAULT NULL, history LONGTEXT NOT NULL, created_at DATETIME NOT NULL, last_updated_at DATETIME NOT NULL, description LONGTEXT NOT NULL, video VARCHAR(255), slug VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_7CC7DA2C989D9B62 (slug), UNIQUE INDEX UNIQ_7CC7DA2C5E237E06 (name), INDEX IDX_7CC7DA2C61220EA6 (creator_id), INDEX IDX_7CC7DA2C896DBBDE (updated_by_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE video ADD CONSTRAINT FK_7CC7DA2C61220EA6 FOREIGN KEY (creator_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE video ADD CONSTRAINT FK_7CC7DA2C896DBBDE FOREIGN KEY (updated_by_id) REFERENCES users (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_140AB620989D9B62 ON page (slug)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_140AB6205E237E06 ON page (name)');
     }
 
     public function down(Schema $schema): void
@@ -29,7 +27,5 @@ final class Version20180606165625 extends AbstractMigration
 
         $this->addSql('DROP TABLE video');
         $this->addSql('ALTER TABLE access_log CHANGE created_at created_at DATETIME DEFAULT \'2017-10-31 00:00:00\' NOT NULL');
-        $this->addSql('DROP INDEX UNIQ_140AB620989D9B62 ON page');
-        $this->addSql('DROP INDEX UNIQ_140AB6205E237E06 ON page');
     }
 }
