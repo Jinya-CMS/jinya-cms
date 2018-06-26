5f02f5c652ec97502d057814f397cc772102ca99
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-06-26 09:06:55 +0200

    Added VideoGallery and Position services

--- src/Formatter/Video/VideoFormatterInterface.php
+++ src/Formatter/Video/VideoFormatterInterface.php
@@ -76,4 +76,12 @@ interface VideoFormatterInterface extends FormatterInterface
      * @return VideoFormatterInterface
      */
     public function poster(): self;
+
+    /**
+     * Formats the galleries
+     *
+     * @return VideoFormatterInterface
+     */
+    public function galleries(): self;
+
 }
---
+++ src/Formatter/Video/VideoPositionFormatter.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imanu
+ * Date: 18.02.2018
+ * Time: 16:20
+ */
+
+namespace Jinya\Formatter\Video;
+
+use Jinya\Entity\Galleries\VideoPosition;
+use Jinya\Formatter\FormatterInterface;
+use Jinya\Formatter\Gallery\VideoGalleryFormatterInterface;
+
+class VideoPositionFormatter implements VideoPositionFormatterInterface
+{
+    /** @var array */
+    private $formattedData;
+
+    /** @var VideoPosition */
+    private $videoPosition;
+
+    /** @var VideoGalleryFormatterInterface */
+    private $galleryFormatterInterface;
+
+    /** @var VideoFormatterInterface */
+    private $videoFormatterInterface;
+
+    /**
+     * @param VideoGalleryFormatterInterface $galleryFormatterInterface
+     */
+    public function setGalleryFormatterInterface(VideoGalleryFormatterInterface $galleryFormatterInterface): void
+    {
+        $this->galleryFormatterInterface = $galleryFormatterInterface;
+    }
+
+    /**
+     * @param VideoFormatterInterface $videoFormatterInterface
+     */
+    public function setVideoFormatterInterface(VideoFormatterInterface $videoFormatterInterface): void
+    {
+        $this->videoFormatterInterface = $videoFormatterInterface;
+    }
+
+    /**
+     * Initializes the format
+     *
+     * @param VideoPosition $videoPosition
+     * @return VideoPositionFormatterInterface
+     */
+    public function init(VideoPosition $videoPosition): VideoPositionFormatterInterface
+    {
+        $this->formattedData = [];
+        $this->videoPosition = $videoPosition;
+
+        return $this;
+    }
+
+    /**
+     * Formats the video
+     *
+     * @return VideoPositionFormatterInterface
+     */
+    public function video(): VideoPositionFormatterInterface
+    {
+        $this->formattedData['video'] = $this->videoFormatterInterface
+            ->init($this->videoPosition->getVideo())
+            ->slug()
+            ->name()
+            ->picture()
+            ->description()
+            ->format();
+
+        return $this;
+    }
+
+    /**
+     * Formats the gallery
+     *
+     * @return VideoPositionFormatterInterface
+     */
+    public function gallery(): VideoPositionFormatterInterface
+    {
+        $this->formattedData['gallery'] = $this->galleryFormatterInterface
+            ->init($this->videoPosition->getGallery())
+            ->slug()
+            ->name()
+            ->description()
+            ->format();
+
+        return $this;
+    }
+
+    /**
+     * Formats the position
+     *
+     * @return VideoPositionFormatterInterface
+     */
+    public function position(): VideoPositionFormatterInterface
+    {
+        $this->formattedData['id'] = $this->videoPosition->getPosition();
+
+        return $this;
+    }
+
+    /**
+     * Formats the id
+     *
+     * @return VideoPositionFormatterInterface
+     */
+    public function id(): VideoPositionFormatterInterface
+    {
+        $this->formattedData['id'] = $this->videoPosition->getId();
+
+        return $this;
+    }
+
+    /**
+     * Formats the content of the @see FormatterInterface into an array
+     *
+     * @return array
+     */
+    public function format(): array
+    {
+        return $this->formattedData;
+    }
+}
