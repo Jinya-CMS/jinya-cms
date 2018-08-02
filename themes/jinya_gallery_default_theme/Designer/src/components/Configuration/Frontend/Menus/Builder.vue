5ae4d78aa7c4598114895d57af292ac37f63b867
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-08-02 15:22:28 +0200

    JGCMS-22 Increase contrast
    Updated look and feel of delete tray

--- themes/jinya_gallery_default_theme/Designer/src/components/Configuration/Frontend/Menus/Builder.vue
+++ themes/jinya_gallery_default_theme/Designer/src/components/Configuration/Frontend/Menus/Builder.vue
@@ -3,9 +3,8 @@
     <jinya-loader :loading="loading"/>
     <jinya-form v-if="!loading" save-label="configuration.frontend.menus.builder.save" @submit="save" @back="back"
                 cancel-label="configuration.frontend.menus.builder.cancel" button-bar-padding-right="0.5rem">
-      <draggable v-show="drag" class="jinya-menu-builder__trash" :options="itemsOptions">
-        <i class="mdi mdi-delete is--big"></i>
-        <span>{{'configuration.frontend.menus.builder.delete'|jmessage}}</span>
+      <draggable v-show="drag" :data-message="'configuration.frontend.menus.builder.delete'|jmessage"
+                 class="jinya-menu-builder__trash" :options="itemsOptions">
       </draggable>
       <jinya-message :message="message" :state="state"/>
       <jinya-editor-pane>
@@ -439,10 +438,24 @@
     width: 100%;
     display: flex;
     transition: opacity 0.3s;
-    color: $danger;
+    background: $danger;
+    color: $white;
     justify-content: center;
     flex-direction: row;
     align-items: center;
+    min-height: 3rem;
+
+    &:empty {
+      position: relative;
+
+      &::before {
+        position: absolute;
+        content: '\f1c0' attr(data-message);
+        font-family: 'Material Design Icons', $font-family;
+        color: $white;
+        font-size: 1.25rem;
+      }
+    }
   }
 
   .jinya-menu-builder {
--- themes/jinya_gallery_default_theme/Designer/src/framework/Markup/IconButton.vue
+++ themes/jinya_gallery_default_theme/Designer/src/framework/Markup/IconButton.vue
@@ -1,14 +1,14 @@
 <template>
-    <a v-if="href" :href="href" class="jinya-icon-button" :class="additionalClasses">
-        <i class="mdi" :class="`mdi-${icon}`"></i>
-    </a>
-    <router-link v-else-if="to" :to="routeTarget" class="jinya-icon-button" :class="additionalClasses">
-        <i class="mdi" :class="`mdi-${icon}`"></i>
-    </router-link>
-    <button :type="type" v-else @click="$event => $emit('click', $event)" class="jinya-icon-button"
-            :disabled="isDisabled" :class="additionalClasses">
-        <i class="mdi" :class="`mdi-${icon}`"></i>
-    </button>
+  <a v-if="href" :href="href" class="jinya-icon-button" :class="additionalClasses">
+    <i class="mdi" :class="`mdi-${icon}`"></i>
+  </a>
+  <router-link v-else-if="to" :to="routeTarget" class="jinya-icon-button" :class="additionalClasses">
+    <i class="mdi" :class="`mdi-${icon}`"></i>
+  </router-link>
+  <button :type="type" v-else @click="$event => $emit('click', $event)" class="jinya-icon-button"
+          :disabled="isDisabled" :class="additionalClasses">
+    <i class="mdi" :class="`mdi-${icon}`"></i>
+  </button>
 </template>
 
 <script>
@@ -41,11 +41,11 @@
     computed: {
       additionalClasses() {
         return {
-          'is--primary': !this.isDisabled || this.isPrimary,
-          'is--secondary': !this.isDisabled || this.isSecondary,
-          'is--danger': !this.isDisabled || this.isDanger,
-          'is--success': !this.isDisabled || this.isSuccess,
-          'is--default': !this.isDisabled || !(this.isSuccess || this.isDanger || this.isPrimary || this.isSecondary),
+          'is--primary': !this.isDisabled && this.isPrimary,
+          'is--secondary': !this.isDisabled && this.isSecondary,
+          'is--danger': !this.isDisabled && this.isDanger,
+          'is--success': !this.isDisabled && this.isSuccess,
+          'is--default': !this.isDisabled && !(this.isSuccess || this.isDanger || this.isPrimary || this.isSecondary),
           'is--inverse': true,
           'is--disabled': this.isDisabled,
         };
@@ -62,25 +62,25 @@
 </script>
 
 <style scoped lang="scss">
-    .jinya-icon-button {
-        border-width: 0;
-        display: inline-block;
-        padding: 0.5em 0.5em;
-        vertical-align: middle;
-        font-size: 100%;
-        transition: background-color 0.3s, color 0.3s;
-        text-align: center;
-        text-decoration: none;
-        cursor: pointer;
+  .jinya-icon-button {
+    border-width: 0;
+    display: inline-block;
+    padding: 0.5em 0.5em;
+    vertical-align: middle;
+    font-size: 100%;
+    transition: background-color 0.3s, color 0.3s;
+    text-align: center;
+    text-decoration: none;
+    cursor: pointer;
 
-        &:focus {
-            outline: none;
-        }
-
-        &:hover {
-            text-decoration: none;
-        }
-
-        @include button-variations(transparent);
+    &:focus {
+      outline: none;
     }
+
+    &:hover {
+      text-decoration: none;
+    }
+
+    @include button-variations(transparent);
+  }
 </style>
