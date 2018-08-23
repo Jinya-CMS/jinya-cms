a0cfd4e83b1128f75218e6032ecdd1539e5e679d
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-08-23 21:42:23 +0200

    JGCMS-26 Jinya Events
    Added form item events

---
+++ src/Framework/Events/Form/FormItemGetItemEvent.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: imanuel
+ * Date: 23.08.18
+ * Time: 21:37
+ */
+
+namespace Jinya\Framework\Events\Form;
+
+use Symfony\Component\EventDispatcher\Event;
+
+class FormItemGetItemEvent extends Event
+{
+    public const PRE_GET = 'FormItemPreGet';
+
+    /** @var string */
+    private $formSlug;
+
+    /** @var integer */
+    private $position;
+
+    /**
+     * FormItemGetItemEvent constructor.
+     * @param string $formSlug
+     * @param int $position
+     */
+    public function __construct(string $formSlug, int $position)
+    {
+        $this->formSlug = $formSlug;
+        $this->position = $position;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFormSlug(): string
+    {
+        return $this->formSlug;
+    }
+
+    /**
+     * @return int
+     */
+    public function getPosition(): int
+    {
+        return $this->position;
+    }
+}
