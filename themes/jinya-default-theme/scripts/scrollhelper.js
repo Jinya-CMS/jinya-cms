e507cf44ff2749e7067adbf2e2a7a68ac6af23d1
Imanuel Ulbricht <imanuel@ulbricht.codes>
2018-12-21 00:51:11 +0100

    JGCMS-45 Create new default theme
    Added styling for mobile galleries

--- themes/jinya-default-theme/scripts/scrollhelper.js
+++ themes/jinya-default-theme/scripts/scrollhelper.js
@@ -1,12 +1,24 @@
 !document.addEventListener('DOMContentLoaded', () => {
   const scrollItem = document.querySelector('.is--horizontal');
+
+  function scrollHandler(e) {
+    if (!e.deltaX) {
+      scrollItem.scrollBy({
+        behavior: 'auto',
+        left: e.deltaY > 0 ? 100 : -100,
+      });
+    }
+  }
+
   if (scrollItem) {
-    scrollItem.addEventListener('wheel', (e) => {
-      if (!e.deltaX) {
-        scrollItem.scrollBy({
-          behavior: 'auto',
-          left: e.deltaY > 0 ? 100 : -100,
-        });
+    if (window.innerWidth >= 1280) {
+      scrollItem.addEventListener('wheel', scrollHandler);
+    }
+    document.addEventListener('resize', () => {
+      if (window.innerWidth < 1280) {
+        scrollItem.removeEventListener('wheel', scrollHandler);
+      } else {
+        scrollItem.addEventListener('wheel', scrollHandler);
       }
     });
   }
