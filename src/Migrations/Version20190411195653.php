7efaedbb64a17312afb6ca9742aea33a58c95c21
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2019-04-11 21:59:48 +0200

    JGCMS-1 Tiling gallery
    Added masonry flag

---
+++ src/Migrations/Version20190411195653.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190411195653 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE video_gallery ADD masonry TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE art_gallery ADD masonry TINYINT(1) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE art_gallery DROP masonry');
+        $this->addSql('ALTER TABLE video_gallery DROP masonry');
+    }
+}
