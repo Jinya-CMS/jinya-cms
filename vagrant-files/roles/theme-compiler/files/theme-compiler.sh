a88e5083854d084785a3f4a104f71ef71082e605
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2019-05-11 02:10:46 +0200

    JGCMS-1 Tiling gallery
    Fixed compilation services

--- vagrant-files/roles/theme-compiler/files/theme-compiler.sh
+++ vagrant-files/roles/theme-compiler/files/theme-compiler.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-while inotifywait -r -e modify,create /vagrant/themes/; do
-    php /vagrant/bin/console jinya:theme:compile
-    php /vagrant/bin/console jinya:cache:compile
+while inotifywait -r -e modify,create /var/www/jinya/themes/; do
+    cd /var/www/jinya/themes/jinya-default-theme/
+    /var/www/jinya/themes/node_modules/.bin/gulp
+    cd /var/www/jinya
+    php /var/www/jinya/bin/console jinya:theme:compile
+    php /var/www/jinya/bin/console jinya:cache:compile
 done
