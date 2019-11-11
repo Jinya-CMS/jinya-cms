3f69677ef69f428640130631294177bddf82aed1
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2019-11-11 06:22:58 +0000

    Apply fixes from StyleCI

--- src/Controller/Api/Form/FormController.php
+++ src/Controller/Api/Form/FormController.php
@@ -136,11 +136,7 @@ class FormController extends BaseApiController
 
             $emailValidator = new EmailValidator();
             if (!$emailValidator->isValid($toAddress, new RFCValidation())) {
-                throw new ValidatorException($translator->trans(
-                    'api.form.field.toAddress.invalid',
-                    ['toAddress' => $toAddress],
-                    'validators'
-                ));
+                throw new ValidatorException($translator->trans('api.form.field.toAddress.invalid', ['toAddress' => $toAddress], 'validators'));
             }
 
             $form = new Form();
@@ -204,11 +200,7 @@ class FormController extends BaseApiController
 
             $emailValidator = new EmailValidator();
             if (!$emailValidator->isValid($toAddress, new RFCValidation())) {
-                throw new ValidatorException($translator->trans(
-                    'api.form.field.toAddress.invalid',
-                    ['toAddress' => $toAddress],
-                    'validators'
-                ));
+                throw new ValidatorException($translator->trans('api.form.field.toAddress.invalid', ['toAddress' => $toAddress], 'validators'));
             }
 
             $form->setName($name);
