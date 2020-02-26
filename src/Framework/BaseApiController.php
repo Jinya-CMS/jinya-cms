4a3c5313c36368d486241b007d5ff53c3890d4b3
Imanuel Ulbricht <imanuel.ulbricht@outlook.com>
2020-02-26 21:35:07 +0000

    Apply fixes from StyleCI

--- src/Framework/BaseApiController.php
+++ src/Framework/BaseApiController.php
@@ -166,9 +166,7 @@ abstract class BaseApiController extends BaseController
 
                 break;
             default:
-                throw new InvalidContentTypeException($this->contentType ?? '',
-                    $this->translator->trans('api.generic.headers.contenttype', ['contentType' => $this->contentType],
-                        'validators'));
+                throw new InvalidContentTypeException($this->contentType ?? '', $this->translator->trans('api.generic.headers.contenttype', ['contentType' => $this->contentType], 'validators'));
         }
 
         return $result;
