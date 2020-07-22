61fcd0c07319b3e58745d74e935feb1a2d90546e
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2020-07-22 21:46:39 +0200

    CHORE: Fixed cors handling

--- src/Web/ResponseEmitter/ResponseEmitter.php
+++ src/Web/ResponseEmitter/ResponseEmitter.php
@@ -13,15 +13,7 @@ class ResponseEmitter extends SlimResponseEmitter
      */
     public function emit(ResponseInterface $response): void
     {
-        // This variable should be set to the allowed host from which your API can be accessed with
-        $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
-
         $newResponse = $response
-            ->withHeader('Access-Control-Allow-Credentials', 'true')
-            ->withHeader('Access-Control-Allow-Origin', $origin)
-            ->withHeader('Access-Control-Allow-Headers',
-                'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-            ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->withHeader('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
             ->withAddedHeader('Cache-Control', 'post-check=0, pre-check=0')
             ->withHeader('Pragma', 'no-cache');
