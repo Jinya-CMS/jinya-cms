e044b16cb80ee1d068d2c40dc9d31e212e966200
Imanuel Ulbricht <imanuel@ulbricht.codes>
2021-01-10 22:26:27 +0100

    Removed old code

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cp -r /var/www/jinya/* /var/www/html/
-touch /var/www/html/.env
-mkdir /var/www/html/public/public/
-chown -R www-data:www-data /var/www/html/
-apache2-foreground
-exec "$@"
