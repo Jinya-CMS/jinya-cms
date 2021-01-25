8cbc22f4801af7b56c13e1353944fb1552ab35c4
Imanuel Ulbricht <imanuel@ulbricht.codes>
2021-01-25 23:07:30 +0100

    JGC-61 Update via CLI
    Execute migrations on new docker container creation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,5 +3,7 @@ cp -r /var/www/jinya/* /var/www/html/
 touch /var/www/html/.env
 mkdir /var/www/html/public/public/
 chown -R www-data:www-data /var/www/html/
+chmod +x /var/www/html/cli/console
+php /var/www/html/cli/console migrate
 apache2-foreground
 exec "$@"
--- src/Database/Migrations/brute-force-prevention.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'version' => 'brute-force-prevention',
-    'sql' => <<<'SQL'
-ALTER TABLE users ADD COLUMN failed_login_attempts int NULL;
-ALTER TABLE users ADD COLUMN login_blocked_until datetime NULL;
-SQL,
-];
