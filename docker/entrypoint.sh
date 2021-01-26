7233ed3d9e0fc4fb41a40e6f3382f162c75986b1
Imanuel Ulbricht <imanuel@ulbricht.codes>
2021-01-26 22:33:05 +0100

    CHORE: Don't create .env file

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 cp -r /var/www/jinya/* /var/www/html/
-touch /var/www/html/.env
 mkdir /var/www/html/public/public/
 chown -R www-data:www-data /var/www/html/
 chmod +x /var/www/html/cli/console
