769d032442cbb042cd99236ad31840f113c29863
Imanuel Ulbricht <imanuel@ulbricht.codes>
2021-02-05 20:28:36 +0100

    JGC-66 Add stats for system data
    Added storage statistics

---
+++ src/Statistics/Storage.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Statistics;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+
+class Storage
+{
+    public function getTotalStorage(): float
+    {
+        return disk_total_space(__ROOT__);
+    }
+
+    public function getFreeStorage(): float
+    {
+        return disk_free_space(__ROOT__);
+    }
+
+    public function getUsedStorage(): float
+    {
+        $size = 0;
+        foreach (
+            new RecursiveIteratorIterator(
+                new RecursiveDirectoryIterator(__ROOT__)
+            ) as $file
+        ) {
+            $size += $file->getSize();
+        }
+        return $size;
+    }
+}
