00b600b14b0c78ba9dbc54b615758ef8a655f34b
Imanuel Ulbricht <me@imanuel.dev>
2021-02-14 23:17:19 +0100

    JGC-76 Load diagrams in parallel
    Load diagrams in parallel

--- designer/src/app/statistics/AccessStatisticsView.svelte
+++ designer/src/app/statistics/AccessStatisticsView.svelte
@@ -36,195 +36,203 @@
       show: false,
     };
 
-    const countryStats = await get('/api/statistics/visits/country');
-    const countryStatsChart = new ApexCharts(country, {
-      chart: {...chart, width: '99%', type: 'treemap'},
-      plotOptions: {
-        treemap: {
-          distributed: true,
-          enableShades: false,
-        },
-      },
-      dataLabels: {
-        formatter(text, {value}) {
-          return `${text}: ${value}`;
-        },
-      },
-      legend,
-      series: [
-        {
-          name: $_('statistics.access.country'),
-          data: countryStats.sort((a, b) => b.visitCount - a.visitCount).map(item => ({
-            y: item.visitCount,
-            x: item.label,
-          })),
-        },
-      ],
-      title:
-        {
-          text: $_('statistics.access.country'),
-          align: 'left',
-          style:
+    await Promise.all([
+      (async () => {
+        const countryStats = await get('/api/statistics/visits/country');
+        const countryStatsChart = new ApexCharts(country, {
+          chart: {...chart, width: '99%', type: 'treemap'},
+          plotOptions: {
+            treemap: {
+              distributed: true,
+              enableShades: false,
+            },
+          },
+          dataLabels: {
+            formatter(text, {value}) {
+              return `${text}: ${value}`;
+            },
+          },
+          legend,
+          series: [
             {
+              name: $_('statistics.access.country'),
+              data: countryStats.sort((a, b) => b.visitCount - a.visitCount).map(item => ({
+                y: item.visitCount,
+                x: item.label,
+              })),
+            },
+          ],
+          title:
+            {
+              text: $_('statistics.access.country'),
+              align: 'left',
+              style:
+                {
+                  fontFamily: 'Lato, sans-serif',
+                  color: 'var(--black)'
+                }
+              ,
+            },
+        });
+        await countryStatsChart.render();
+      })(),
+      (async () => {
+        const languageStats = await get('/api/statistics/visits/language');
+        const languageStatsChart = new ApexCharts(language, {
+          chart: {...chart, width: '32%'},
+          plotOptions,
+          dataLabels,
+          legend,
+          series: [
+            {
+              name: $_('statistics.access.language'),
+              data: languageStats.map(item => item.visitCount),
+            },
+          ],
+          xaxis: {
+            categories: languageStats.map(item => item.label),
+            style: {
+              fontSize: '12px',
+              fontFamily: 'Lato, sans-serif',
+            },
+          },
+          labels: languageStats.map(item => item.label),
+          title: {
+            text: $_('statistics.access.language'),
+            align: 'left',
+            style: {
               fontFamily: 'Lato, sans-serif',
               color: 'var(--black)'
-            }
-          ,
-        },
-    });
-    await countryStatsChart.render();
-
-    const languageStats = await get('/api/statistics/visits/language');
-    const languageStatsChart = new ApexCharts(language, {
-      chart: {...chart, width: '32%'},
-      plotOptions,
-      dataLabels,
-      legend,
-      series: [
-        {
-          name: $_('statistics.access.language'),
-          data: languageStats.map(item => item.visitCount),
-        },
-      ],
-      xaxis: {
-        categories: languageStats.map(item => item.label),
-        style: {
-          fontSize: '12px',
-          fontFamily: 'Lato, sans-serif',
-        },
-      },
-      labels: languageStats.map(item => item.label),
-      title: {
-        text: $_('statistics.access.language'),
-        align: 'left',
-        style: {
-          fontFamily: 'Lato, sans-serif',
-          color: 'var(--black)'
-        },
-      },
-    });
-    await languageStatsChart.render();
-
-    const deviceBrandStats = await get('/api/statistics/visits/brand');
-    const deviceBrandStatsChart = new ApexCharts(deviceBrand, {
-      chart: {...chart, width: '32%'},
-      plotOptions,
-      dataLabels,
-      legend,
-      series: [
-        {
-          name: $_('statistics.access.device_brand'),
-          data: deviceBrandStats.map(item => item.visitCount),
-        },
-      ],
-      xaxis: {
-        categories: deviceBrandStats.map(item => item.label),
-        style: {
-          fontSize: '12px',
-          fontFamily: 'Lato, sans-serif',
-        },
-      },
-      labels: deviceBrandStats.map(item => item.label),
-      title: {
-        text: $_('statistics.access.device_brand'),
-        align: 'left',
-        style: {
-          fontFamily: 'Lato, sans-serif',
-          color: 'var(--black)'
-        },
-      },
-    });
-    await deviceBrandStatsChart.render();
-
-    const deviceTypeStats = await get('/api/statistics/visits/type');
-    const deviceTypeStatsChart = new ApexCharts(deviceType, {
-      chart: {
-        type: 'pie',
-        height: 400,
-        width: '33%',
-      },
-      dataLabels: {
-        distributed: true,
-      },
-      legend: {
-        width: 100,
-        fontFamily: 'Lato, sans-serif',
-      },
-      series: deviceTypeStats.filter(item => item.visitCount > 0).map(item => item.visitCount),
-      labels: deviceTypeStats.filter(item => item.visitCount > 0).map(item => item.label),
-      title: {
-        text: $_('statistics.access.device_type'),
-        align: 'left',
-        style: {
-          fontFamily: 'Lato, sans-serif',
-          color: 'var(--black)'
-        },
-      },
-    });
-    await deviceTypeStatsChart.render();
-
-    const browserStats = await get('/api/statistics/visits/browser');
-    const browserStatsChart = new ApexCharts(browser, {
-      chart,
-      plotOptions,
-      dataLabels,
-      legend,
-      series: [
-        {
-          name: $_('statistics.access.browser'),
-          data: browserStats.map(item => item.visitCount),
-        },
-      ],
-      xaxis: {
-        categories: browserStats.map(item => item.label),
-        style: {
-          fontSize: '12px',
-          fontFamily: 'Lato, sans-serif',
-        },
-      },
-      labels: browserStats.map(item => item.label),
-      title: {
-        text: $_('statistics.access.browser'),
-        align: 'left',
-        style: {
-          fontFamily: 'Lato, sans-serif',
-          color: 'var(--black)'
-        },
-      },
-    });
-    await browserStatsChart.render();
-
-    const operatingSystemStats = await get('/api/statistics/visits/os');
-    const operatingSystemStatsChart = new ApexCharts(operatingSystem, {
-      chart,
-      plotOptions,
-      dataLabels,
-      legend,
-      series: [
-        {
-          name: $_('statistics.access.operating_system'),
-          data: operatingSystemStats.map(item => item.visitCount),
-          style: {
-            fontSize: '12px',
+            },
+          },
+        });
+        await languageStatsChart.render();
+      })(),
+      (async () => {
+        const deviceBrandStats = await get('/api/statistics/visits/brand');
+        const deviceBrandStatsChart = new ApexCharts(deviceBrand, {
+          chart: {...chart, width: '32%'},
+          plotOptions,
+          dataLabels,
+          legend,
+          series: [
+            {
+              name: $_('statistics.access.device_brand'),
+              data: deviceBrandStats.map(item => item.visitCount),
+            },
+          ],
+          xaxis: {
+            categories: deviceBrandStats.map(item => item.label),
+            style: {
+              fontSize: '12px',
+              fontFamily: 'Lato, sans-serif',
+            },
+          },
+          labels: deviceBrandStats.map(item => item.label),
+          title: {
+            text: $_('statistics.access.device_brand'),
+            align: 'left',
+            style: {
+              fontFamily: 'Lato, sans-serif',
+              color: 'var(--black)'
+            },
+          },
+        });
+        await deviceBrandStatsChart.render();
+      })(),
+      (async () => {
+        const deviceTypeStats = await get('/api/statistics/visits/type');
+        const deviceTypeStatsChart = new ApexCharts(deviceType, {
+          chart: {
+            type: 'pie',
+            height: 400,
+            width: '33%',
+          },
+          dataLabels: {
+            distributed: true,
+          },
+          legend: {
+            width: 100,
             fontFamily: 'Lato, sans-serif',
           },
-        },
-      ],
-      xaxis: {
-        categories: operatingSystemStats.map(item => item.label),
-      },
-      labels: operatingSystemStats.map(item => item.label),
-      title: {
-        text: $_('statistics.access.operating_system'),
-        align: 'left',
-        style: {
-          fontFamily: 'Lato, sans-serif',
-          color: 'var(--black)'
-        },
-      },
-    });
-    await operatingSystemStatsChart.render();
-
+          series: deviceTypeStats.filter(item => item.visitCount > 0).map(item => item.visitCount),
+          labels: deviceTypeStats.filter(item => item.visitCount > 0).map(item => item.label),
+          title: {
+            text: $_('statistics.access.device_type'),
+            align: 'left',
+            style: {
+              fontFamily: 'Lato, sans-serif',
+              color: 'var(--black)'
+            },
+          },
+        });
+        await deviceTypeStatsChart.render();
+      })(),
+      (async () => {
+        const browserStats = await get('/api/statistics/visits/browser');
+        const browserStatsChart = new ApexCharts(browser, {
+          chart,
+          plotOptions,
+          dataLabels,
+          legend,
+          series: [
+            {
+              name: $_('statistics.access.browser'),
+              data: browserStats.map(item => item.visitCount),
+            },
+          ],
+          xaxis: {
+            categories: browserStats.map(item => item.label),
+            style: {
+              fontSize: '12px',
+              fontFamily: 'Lato, sans-serif',
+            },
+          },
+          labels: browserStats.map(item => item.label),
+          title: {
+            text: $_('statistics.access.browser'),
+            align: 'left',
+            style: {
+              fontFamily: 'Lato, sans-serif',
+              color: 'var(--black)'
+            },
+          },
+        });
+        await browserStatsChart.render();
+      })(),
+      (async () => {
+        const operatingSystemStats = await get('/api/statistics/visits/os');
+        const operatingSystemStatsChart = new ApexCharts(operatingSystem, {
+          chart,
+          plotOptions,
+          dataLabels,
+          legend,
+          series: [
+            {
+              name: $_('statistics.access.operating_system'),
+              data: operatingSystemStats.map(item => item.visitCount),
+              style: {
+                fontSize: '12px',
+                fontFamily: 'Lato, sans-serif',
+              },
+            },
+          ],
+          xaxis: {
+            categories: operatingSystemStats.map(item => item.label),
+          },
+          labels: operatingSystemStats.map(item => item.label),
+          title: {
+            text: $_('statistics.access.operating_system'),
+            align: 'left',
+            style: {
+              fontFamily: 'Lato, sans-serif',
+              color: 'var(--black)'
+            },
+          },
+        });
+        await operatingSystemStatsChart.render();
+      })(),
+    ]);
   });
 
   const to = new Date(Date.now());
--- designer/src/app/statistics/DatabaseStatisticsView.svelte
+++ designer/src/app/statistics/DatabaseStatisticsView.svelte
@@ -9,85 +9,91 @@
   let systemStats;
 
   onMount(async () => {
-    const entityShareData = await get('/api/statistics/entity');
-    const entityShareChart = new ApexCharts(entityShare, {
-      chart: {
-        type: 'pie',
-        height: 400,
-        width: 500,
-      },
-      series: [
-        entityShareData.galleries,
-        entityShareData.simplePages,
-        entityShareData.segmentPages,
-        entityShareData.forms,
-      ],
-      labels: [
-        $_('statistics.database.galleries'),
-        $_('statistics.database.simple_pages'),
-        $_('statistics.database.segment_pages'),
-        $_('statistics.database.forms'),
-      ],
-    });
-    await entityShareChart.render();
-
-    const systemStatsData = await get('/api/statistics/system');
-    const systemStatsChart = new ApexCharts(systemStats, {
-      chart: {
-        type: 'pie',
-        height: 400,
-        width: 500,
-      },
-      dataLabels: {
-        formatter(val, {seriesIndex, w}) {
-          return `${(w.config.series[seriesIndex] / 1024 / 1024 / 1024).toFixed(2)} GB`
-        },
-      },
-      series: [
-        systemStatsData.total - systemStatsData.free,
-        systemStatsData.free,
-      ],
-      labels: [
-        $_('statistics.system.used'),
-        $_('statistics.system.free'),
-      ],
-    });
-    await systemStatsChart.render();
-
-    const fileHistoryData = await get('/api/statistics/history/file');
-    const fileHistoryChart = new ApexCharts(fileHistory, {
-      series: [
-        {
-          name: $_('statistics.database.history.created'),
-          data: fileHistoryData.created.map((item) => ({x: new Date(item.date), y: item.count})),
-        },
-        {
-          name: $_('statistics.database.history.updated'),
-          data: fileHistoryData.updated.map((item) => ({x: new Date(item.date), y: item.count})),
-        },
-      ],
-      chart: {
-        height: 400,
-        type: 'area',
-        zoom: {
-          enabled: false
-        },
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      stroke: {
-        curve: 'smooth',
-      },
-      title: {
-        text: $_('statistics.database.history.file'),
-        align: 'left',
-      },
-      xaxis: {
-        type: 'datetime',
-      }
-    });
-    await fileHistoryChart.render();
+    await Promise.all([
+      (async () => {
+        const entityShareData = await get('/api/statistics/entity');
+        const entityShareChart = new ApexCharts(entityShare, {
+          chart: {
+            type: 'pie',
+            height: 400,
+            width: 500,
+          },
+          series: [
+            entityShareData.galleries,
+            entityShareData.simplePages,
+            entityShareData.segmentPages,
+            entityShareData.forms,
+          ],
+          labels: [
+            $_('statistics.database.galleries'),
+            $_('statistics.database.simple_pages'),
+            $_('statistics.database.segment_pages'),
+            $_('statistics.database.forms'),
+          ],
+        });
+        await entityShareChart.render();
+      })(),
+      (async () => {
+        const systemStatsData = await get('/api/statistics/system');
+        const systemStatsChart = new ApexCharts(systemStats, {
+          chart: {
+            type: 'pie',
+            height: 400,
+            width: 500,
+          },
+          dataLabels: {
+            formatter(val, {seriesIndex, w}) {
+              return `${(w.config.series[seriesIndex] / 1024 / 1024 / 1024).toFixed(2)} GB`
+            },
+          },
+          series: [
+            systemStatsData.total - systemStatsData.free,
+            systemStatsData.free,
+          ],
+          labels: [
+            $_('statistics.system.used'),
+            $_('statistics.system.free'),
+          ],
+        });
+        await systemStatsChart.render();
+      })(),
+      (async () => {
+        const fileHistoryData = await get('/api/statistics/history/file');
+        const fileHistoryChart = new ApexCharts(fileHistory, {
+          series: [
+            {
+              name: $_('statistics.database.history.created'),
+              data: fileHistoryData.created.map((item) => ({x: new Date(item.date), y: item.count})),
+            },
+            {
+              name: $_('statistics.database.history.updated'),
+              data: fileHistoryData.updated.map((item) => ({x: new Date(item.date), y: item.count})),
+            },
+          ],
+          chart: {
+            height: 400,
+            type: 'area',
+            zoom: {
+              enabled: false
+            },
+          },
+          dataLabels: {
+            enabled: false,
+          },
+          stroke: {
+            curve: 'smooth',
+          },
+          title: {
+            text: $_('statistics.database.history.file'),
+            align: 'left',
+          },
+          xaxis: {
+            type: 'datetime',
+          }
+        });
+        await fileHistoryChart.render();
+      })(),
+    ]);
   });
 </script>
 
