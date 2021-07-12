539669b5819a2c542f81ece00eb8363a4dde9dc3
Imanuel Ulbricht <me@imanuel.dev>
2021-07-12 23:08:53 +0200

    #14 Implement unit tests
    Implemented test for UuidGenerator

---
+++ tests/Utils/UuidGeneratorTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Utils;
+
+use App\Utils\UuidGenerator;
+use Exception;
+use PHPUnit\Framework\TestCase;
+
+class UuidGeneratorTest extends TestCase
+{
+
+    public function testGenerateV4()
+    {
+        try {
+            $uuid = UuidGenerator::generateV4();
+            $this->assertMatchesRegularExpression('/^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/', $uuid, 'UUID matches format');
+        } catch (Exception $e) {
+            $this->fail($e->getMessage());
+        }
+    }
+}
