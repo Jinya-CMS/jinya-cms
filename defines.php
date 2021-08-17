b59063896a727e9a4ba18653c223429acbc680b8
Imanuel Ulbricht <me@imanuel.dev>
2021-08-17 23:09:07 +0200

    #40 jinya-content is not created if missing
    Folder jinya-content gets created on request

--- defines.php
+++ defines.php
@@ -7,3 +7,6 @@ const __JINYA_TEMP = __ROOT__ . '/tmp/';
 if (!is_dir(__JINYA_TEMP)) {
     mkdir(__JINYA_TEMP, 0775, true);
 }
+if (!is_dir(__ROOT__ . '/public/jinya-content')) {
+    mkdir(__ROOT__ . '/public/jinya-content', 0775, true);
+}
