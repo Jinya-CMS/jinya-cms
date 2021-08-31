451ee07a087c5c13ae0a37fd71078d4c4fdbd1e2
Imanuel Ulbricht <me@imanuel.dev>
2021-08-31 23:41:50 +0200

    TG-80 Category managment
    Added list action for categories

---
+++ tests/Web/Actions/Blog/ListAllBlogCategoriesActionTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Jinya\Tests\Web\Actions\Blog;
+
+use App\Database\BlogCategory;
+use App\Web\Actions\Action;
+use App\Web\Actions\Blog\Category\ListAllBlogCategoriesAction;
+use Faker\Provider\Uuid;
+use Nyholm\Psr7\Response;
+use PHPUnit\Framework\TestCase;
+
+class ListAllBlogCategoriesActionTest extends TestCase
+{
+
+    /**
+     * @var array<BlogCategory>
+     */
+    private array $categories = [];
+
+    public function setUp(): void
+    {
+        $this->categories = [];
+        for ($i = 0; $i < random_int(5, 20); $i++) {
+            $cat = new BlogCategory();
+            $cat->description = Uuid::uuid();
+            $cat->name = Uuid::uuid();
+            $cat->create();
+            $this->categories[] = $cat;
+        }
+    }
+
+    public function testAction(): void
+    {
+        $action = new ListAllBlogCategoriesAction();
+        $action->response = new Response();
+        $response = $action->action();
+        $this->assertEquals(Action::HTTP_OK, $response->getStatusCode());
+
+        $response->getBody()->rewind();
+        $body = $response->getBody()->getContents();
+        $this->assertNotEmpty($body);
+
+        $json = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
+        $this->assertNotEmpty($json['items']);
+        $this->assertCount(count($this->categories), $json['items']);
+    }
+
+    public function testActionByKeyword(): void
+    {
+        $action = new ListAllBlogCategoriesAction();
+        $action->response = new Response();
+        $action->queryParams = ['keyword' => $this->categories[0]->description];
+        $response = $action->action();
+        $this->assertEquals(Action::HTTP_OK, $response->getStatusCode());
+
+        $response->getBody()->rewind();
+        $body = $response->getBody()->getContents();
+        $this->assertNotEmpty($body);
+
+        $json = json_decode($body, true, 512, JSON_THROW_ON_ERROR);
+        $this->assertNotEmpty($json['items']);
+        $this->assertCount(1, $json['items']);
+    }
+}
