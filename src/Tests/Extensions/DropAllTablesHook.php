c228f4f1f463537af7543848b5dd343f8177437b
Imanuel Ulbricht <me@imanuel.dev>
2022-06-25 01:29:47 +0200

    TG-145 Update Jenkinsfile for MySQL, MariaDB and Percona
    Fix table drop from message

--- src/Tests/Extensions/DropAllTablesHook.php
+++ src/Tests/Extensions/DropAllTablesHook.php
@@ -13,34 +13,35 @@ class DropAllTablesHook implements AfterLastTestHook
 
     public function executeAfterLastTest(): void
     {
-        LoadableEntity::executeSqlString('drop table api_key');
-        LoadableEntity::executeSqlString('drop table blog_post_segment');
-        LoadableEntity::executeSqlString('drop table blog_post');
-        LoadableEntity::executeSqlString('drop table configuration');
-        LoadableEntity::executeSqlString('drop table form_item');
-        LoadableEntity::executeSqlString('drop table gallery_file_position');
-        LoadableEntity::executeSqlString('drop table known_device');
-        LoadableEntity::executeSqlString('drop table menu_item');
-        LoadableEntity::executeSqlString('drop table migration_state');
-        LoadableEntity::executeSqlString('drop table segment');
-        LoadableEntity::executeSqlString('drop table theme_asset');
-        LoadableEntity::executeSqlString('drop table theme_blog_category');
-        LoadableEntity::executeSqlString('drop table blog_category');
-        LoadableEntity::executeSqlString('drop table theme_file');
-        LoadableEntity::executeSqlString('drop table theme_form');
-        LoadableEntity::executeSqlString('drop table form');
-        LoadableEntity::executeSqlString('drop table theme_gallery');
-        LoadableEntity::executeSqlString('drop table gallery');
-        LoadableEntity::executeSqlString('drop table theme_menu');
-        LoadableEntity::executeSqlString('drop table menu');
-        LoadableEntity::executeSqlString('drop table theme_page');
-        LoadableEntity::executeSqlString('drop table page');
-        LoadableEntity::executeSqlString('drop table theme_segment_page');
-        LoadableEntity::executeSqlString('drop table segment_page');
-        LoadableEntity::executeSqlString('drop table theme');
-        LoadableEntity::executeSqlString('drop table uploading_file_chunk');
-        LoadableEntity::executeSqlString('drop table uploading_file');
-        LoadableEntity::executeSqlString('drop table file');
-        LoadableEntity::executeSqlString('drop table users');
+        LoadableEntity::executeSqlString('drop table if exists api_key');
+        LoadableEntity::executeSqlString('drop table if exists blog_post_segment');
+        LoadableEntity::executeSqlString('drop table if exists blog_post');
+        LoadableEntity::executeSqlString('drop table if exists configuration');
+        LoadableEntity::executeSqlString('drop table if exists form_item');
+        LoadableEntity::executeSqlString('drop table if exists gallery_file_position');
+        LoadableEntity::executeSqlString('drop table if exists known_device');
+        LoadableEntity::executeSqlString('drop table if exists menu_item');
+        LoadableEntity::executeSqlString('drop table if exists migration_state');
+        LoadableEntity::executeSqlString('drop table if exists segment');
+        LoadableEntity::executeSqlString('drop table if exists theme_asset');
+        LoadableEntity::executeSqlString('drop table if exists theme_blog_category');
+        LoadableEntity::executeSqlString('drop table if exists blog_category');
+        LoadableEntity::executeSqlString('drop table if exists theme_file');
+        LoadableEntity::executeSqlString('drop table if exists theme_form');
+        LoadableEntity::executeSqlString('drop table if exists message');
+        LoadableEntity::executeSqlString('drop table if exists form');
+        LoadableEntity::executeSqlString('drop table if exists theme_gallery');
+        LoadableEntity::executeSqlString('drop table if exists gallery');
+        LoadableEntity::executeSqlString('drop table if exists theme_menu');
+        LoadableEntity::executeSqlString('drop table if exists menu');
+        LoadableEntity::executeSqlString('drop table if exists theme_page');
+        LoadableEntity::executeSqlString('drop table if exists page');
+        LoadableEntity::executeSqlString('drop table if exists theme_segment_page');
+        LoadableEntity::executeSqlString('drop table if exists segment_page');
+        LoadableEntity::executeSqlString('drop table if exists theme');
+        LoadableEntity::executeSqlString('drop table if exists uploading_file_chunk');
+        LoadableEntity::executeSqlString('drop table if exists uploading_file');
+        LoadableEntity::executeSqlString('drop table if exists file');
+        LoadableEntity::executeSqlString('drop table if exists users');
     }
 }
