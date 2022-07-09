609e5c6b8419ddef8f812f581a31f75b9e5fe316
Imanuel Ulbricht <me@imanuel.dev>
2022-07-09 03:14:46 +0200

    TG-14 Implement unit tests
    Added unit test for blog actions

---
+++ tests/Web/Actions/Blog/Post/BatchSegmentsActionTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Jinya\Tests\Web\Actions\Blog\Post;
+
+use App\Database\BlogPost;
+use App\Web\Actions\Blog\Post\BatchSegmentsAction;
+use App\Web\Exceptions\NoResultException;
+use Nyholm\Psr7\Response;
+use Nyholm\Psr7\ServerRequest;
+use PHPUnit\Framework\TestCase;
+
+class BatchSegmentsActionTest extends TestCase
+{
+
+    public function test__invoke(): void
+    {
+        $post = new BlogPost();
+        $post->title = 'Title';
+        $post->slug = 'slug';
+        $post->create();
+
+        $request = new ServerRequest('', '');
+        $request = $request->withParsedBody(['segments' => []]);
+        $response = new Response();
+        $action = new BatchSegmentsAction();
+        $result = $action($request, $response, ['id' => $post->getIdAsInt()]);
+
+        self::assertEquals(204, $result->getStatusCode());
+    }
+
+    public function test__invokePostNotFound(): void
+    {
+        $this->expectException(NoResultException::class);
+        $request = new ServerRequest('', '');
+        $request = $request->withParsedBody(['segments' => []]);
+        $response = new Response();
+        $action = new BatchSegmentsAction();
+        $action($request, $response, ['id' => -1]);
+    }
+}
