e89cf71a9fe6f1770e2eba3c02b479bc7aff2e69
Imanuel Ulbricht <me@imanuel.dev>
2022-07-12 23:09:46 +0200

    TG-14 Implement unit tests
    Added unit test for ip locator action

---
+++ tests/Web/Actions/LocateIp/LocatorActionTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Jinya\Tests\Web\Actions\LocateIp;
+
+use App\Web\Actions\LocateIp\LocatorAction;
+use Nyholm\Psr7\Response;
+use Nyholm\Psr7\ServerRequest;
+use PHPUnit\Framework\TestCase;
+
+class LocatorActionTest extends TestCase
+{
+
+    public function test__invoke(): void
+    {
+        $request = new ServerRequest('', '');
+        $response = new Response();
+        $action = new LocatorAction();
+        $result = $action($request, $response, ['ip' => '185.216.179.123']);
+        $result->getBody()->rewind();
+
+        $body = json_decode($result->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
+        self::assertArrayHasKey('country', $body);
+        self::assertArrayHasKey('region', $body);
+        self::assertArrayHasKey('city', $body);
+
+        self::assertEquals('Germany', $body['country']);
+        self::assertEquals('Baden-Württemberg', $body['region']);
+        self::assertEquals('Karlsruhe', $body['city']);
+    }
+}
