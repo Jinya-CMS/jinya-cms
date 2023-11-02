2dbc723a157423d49eed4901e23d4408198ca268
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2023-11-02 01:54:24 +0100

    TG-200 Tags in files
    Added tag filtering in file picker

--- public/designer/js/foundation/ui/filePicker.js
+++ public/designer/js/foundation/ui/filePicker.js
@@ -1,6 +1,7 @@
 import html from '../../../lib/jinya-html.js';
 import { get } from '../http/request.js';
 import localize from '../localize.js';
+import './components/tag.js';
 
 /**
  * Displays a file picker modal dialog
@@ -17,11 +18,13 @@ export default async function filePicker({
   pickLabel,
 }) {
   const { items: files } = await get('/api/file');
+  const { items: tags } = await get('/api/file-tag');
 
   return new Promise((resolve) => {
     const container = document.createElement('div');
     document.body.appendChild(container);
     const modalId = crypto.randomUUID();
+    const activeTags = new Set();
 
     if (!cancelLabel) {
       // eslint-disable-next-line no-param-reassign
@@ -35,6 +38,27 @@ export default async function filePicker({
         <div class="cosmo-modal jinya-file-picker__modal">
           <h1 class="cosmo-modal__title">${title}</h1>
           <div class="cosmo-modal__content">
+            <div class="jinya-picker__tag-list">
+              <cms-tag
+                class="jinya-tag--file"
+                emoji=""
+                name="${localize({ key: 'media.galleries.action.show_all_tags' })}"
+                color="#19324c"
+                tag-id="-1"
+                id="show-all-tags"
+              ></cms-tag>
+              ${tags.map(
+                (tag) =>
+                  html` <cms-tag
+                    class="jinya-tag--file"
+                    emoji="${tag.emoji}"
+                    name="${tag.name}"
+                    color="${tag.color}"
+                    tag-id="${tag.id}"
+                    id="show-tag-${tag.id}"
+                  ></cms-tag>`,
+              )}
+            </div>
             <div class="jinya-media-tile__container--modal">
               ${files.map(
                 (file) =>
@@ -63,6 +87,47 @@ export default async function filePicker({
 
     document.body.appendChild(container);
 
+    document.querySelectorAll('.jinya-picker__tag-list cms-tag').forEach((tag) =>
+      tag.addEventListener('click', (evt) => {
+        evt.stopPropagation();
+        // eslint-disable-next-line no-param-reassign
+        tag.active = !tag.active;
+        if (tag.id === 'show-all-tags') {
+          container
+            .querySelectorAll('.jinya-media-tile')
+            .forEach((tile) => tile.classList.remove('jinya-media-tile--hidden'));
+          container.querySelectorAll('cms-tag').forEach((t) => {
+            // eslint-disable-next-line no-param-reassign
+            t.active = false;
+          });
+          // eslint-disable-next-line no-param-reassign
+          tag.active = true;
+        } else {
+          const allTags = container.querySelector('#show-all-tags');
+          if (tag.active) {
+            activeTags.add(tag.tagId);
+          } else {
+            activeTags.delete(tag.tagId);
+          }
+          allTags.active = activeTags.size === 0 || activeTags.size === tags.length;
+          container.querySelectorAll('.jinya-media-tile').forEach((tile) => {
+            const file = files.find((f) => f.id === parseInt(tile.getAttribute('data-id'), 10));
+            if (file.tags.filter((f) => activeTags.has(f.id)).length === 0) {
+              tile.classList.add('jinya-media-tile--hidden');
+            } else {
+              tile.classList.remove('jinya-media-tile--hidden');
+            }
+          });
+
+          if (allTags.active) {
+            activeTags.clear();
+            container.querySelectorAll('.jinya-media-tile').forEach((tile) => {
+              tile.classList.remove('jinya-media-tile--hidden');
+            });
+          }
+        }
+      }),
+    );
     container.querySelectorAll('.jinya-media-tile').forEach((item) => {
       item.addEventListener('click', (e) => {
         e.preventDefault();
