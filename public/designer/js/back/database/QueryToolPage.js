0a05f32348b45be5d1cadcfad0683b267b0617ec
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2024-03-24 20:33:30 +0100

    TG-213 Update Cosmo CSS
    Update all stylings in the database area

--- public/designer/js/back/database/QueryToolPage.js
+++ public/designer/js/back/database/QueryToolPage.js
@@ -14,17 +14,18 @@ export default class QueryToolPage extends JinyaDesignerPage {
 
   // eslint-disable-next-line class-methods-use-this
   toString() {
-    return html` <div class="jinya-horizontal-split">
-      <div class="jinya-code-editor__container">
-        <div class="jinya-code-editor"></div>
-        <div class="cosmo-button__container jinya-code-editor__execute">
-          <button class="cosmo-button" id="execute">${localize({ key: 'database.query_tool.execute' })}</button>
+    return html`
+      <div class="jinya-horizontal-split">
+        <div class="jinya-code-editor__container">
+          <div class="jinya-code-editor"></div>
+          <div class="cosmo-button__container jinya-code-editor__execute">
+            <button class="cosmo-button" id="execute">${localize({ key: 'database.query_tool.execute' })}</button>
+          </div>
         </div>
-      </div>
-      <div class="jinya-code-editor__result">
-        <div class="cosmo-tab-control cosmo-tab-control--query-tool"></div>
-      </div>
-    </div>`;
+        <div class="jinya-code-editor__result">
+          <div class="cosmo-tab-control cosmo-tab-control--query-tool"></div>
+        </div>
+      </div>`;
   }
 
   async displayed() {
@@ -32,6 +33,7 @@ export default class QueryToolPage extends JinyaDesignerPage {
     this.editor = monaco.editor.create(document.querySelector('.jinya-code-editor'), {
       language: 'mysql',
       theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? 'hc-black' : 'vs',
+      fontFamily: 'Oxygen Mono',
     });
   }
 
@@ -39,34 +41,34 @@ export default class QueryToolPage extends JinyaDesignerPage {
     const container = document.querySelector('.cosmo-tab-control--query-tool');
     clearChildren({ parent: container });
     const tabs = document.createElement('div');
-    tabs.classList.add('cosmo-tab-control__tabs');
+    tabs.classList.add('cosmo-tab__links');
     container.append(tabs);
 
     let isFirst = true;
     for (const result of this.queryResult) {
       const tabElem = document.createElement('a');
-      tabElem.classList.add('cosmo-tab-control__tab-link');
+      tabElem.classList.add('cosmo-tab__link');
       if (isFirst) {
-        tabElem.classList.add('cosmo-tab-control__tab-link--active');
+        tabElem.classList.add('is--active');
       }
       tabElem.innerText = result.statement;
       tabElem.addEventListener('click', () => {
         document
-          .querySelectorAll('.cosmo-tab-control__tab-link--active')
-          .forEach((item) => item.classList.remove('cosmo-tab-control__tab-link--active'));
-        document.querySelectorAll('.cosmo-tab-control__content')
+          .querySelectorAll('.cosmo-tab__link.is--active')
+          .forEach((item) => item.classList.remove('is--active'));
+        document.querySelectorAll('.cosmo-tab__content')
           .forEach((item) => {
             // eslint-disable-next-line no-param-reassign
             item.style.display = 'none';
           });
-        tabElem.classList.add('cosmo-tab-control__tab-link--active');
+        tabElem.classList.add('is--active');
         document.getElementById(result.statement).style.display = 'flex';
       });
       tabs.append(tabElem);
 
       const contentElem = document.createElement('div');
       container.append(contentElem);
-      contentElem.classList.add('cosmo-tab-control__content');
+      contentElem.classList.add('cosmo-tab__content');
       if (isFirst) {
         contentElem.style.display = 'flex';
       } else {
@@ -77,14 +79,15 @@ export default class QueryToolPage extends JinyaDesignerPage {
         contentElem.innerText = localize({ key: 'database.query_tool.no_result' });
       } else if (result.result.length > 0) {
         const keys = Object.keys(result.result[0]);
-        contentElem.innerHTML = html` <table class="cosmo-table">
-          <thead>
+        contentElem.innerHTML = html`
+          <table class="cosmo-table">
+            <thead>
             <tr>
               ${keys.map((key) => `<th>${key}</th>`)}
             </tr>
-          </thead>
-          <tbody></tbody>
-        </table>`;
+            </thead>
+            <tbody></tbody>
+          </table>`;
         for (const row of result.result) {
           const tr = document.createElement('tr');
           for (const key of keys) {
