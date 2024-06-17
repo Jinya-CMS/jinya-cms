bcce1d5b0e5d96679d1c2579f4f38e005a8d9ff9
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2024-06-18 01:54:02 +0200

    TG-191 Add analytics integrated in Jinya
    Fix unit test with mysql

--- migrations/TG191Analytics.php
+++ migrations/TG191Analytics.php
@@ -11,26 +11,27 @@ class TG191Analytics extends AbstractMigration
     {
         $pdo->exec(
             <<<SQL
-create table analytics (
-    id int primary key auto_increment,
-    route varchar(255) not null,
-    timestamp date default current_date() not null,
-    unique_visit bool default false not null,
-    operating_system varchar(255) null,
+create table analytics
+(
+    id                       int primary key auto_increment,
+    route                    varchar(255) not null,
+    timestamp                date         not null default (current_date),
+    unique_visit             boolean      not null default false,
+    operating_system         varchar(255) null,
     operating_system_version varchar(255) null,
-    browser varchar(255) null,
-    browser_version varchar(255) null,
-    device_type tinyint null,
-    entity_id int null,
-    entity_type int null,
-    language varchar(255) null,
-    device varchar(255) null,
-    brand varchar(255) null,
-    country char(2) null,
-    user_agent varchar(255) null,
-    referer text null,
-    status smallint not null default 200,
-    
+    browser                  varchar(255) null,
+    browser_version          varchar(255) null,
+    device_type              tinyint      null,
+    entity_id                int          null,
+    entity_type              int          null,
+    language                 varchar(255) null,
+    device                   varchar(255) null,
+    brand                    varchar(255) null,
+    country                  char(2)      null,
+    user_agent               varchar(255) null,
+    referer                  text         null,
+    status                   smallint     not null default 200,
+
     index (unique_visit),
     index (timestamp),
     index (route),
