a2d8427dc431dc321c819093cb695e0472bf6eba
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2024-07-13 21:45:56 +0200

    TG-202 Folders for files
    Added api controller to get folder structure for root folder

---
+++ src/Web/Controllers/MediaController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Jinya\Cms\Web\Controllers;
+
+use Jinya\Cms\Database\File;
+use Jinya\Cms\Database\Folder;
+use Jinya\Cms\Web\Middleware\AuthorizationMiddleware;
+use Jinya\Router\Attributes\Controller;
+use Jinya\Router\Attributes\HttpMethod;
+use Jinya\Router\Attributes\Middlewares;
+use Jinya\Router\Attributes\Route;
+use JsonException;
+use Psr\Http\Message\ResponseInterface;
+
+#[Controller]
+class MediaController extends BaseController
+{
+    /**
+     * @return ResponseInterface
+     * @throws JsonException
+     */
+    #[Route(HttpMethod::GET, 'api/media')]
+    #[Middlewares(new AuthorizationMiddleware(ROLE_READER))]
+    public function getRootFolder(): ResponseInterface
+    {
+        $folders = Folder::findRootFolders();
+        $files = File::findRootFiles();
+
+        $data = [
+            'folders' => $this->formatIteratorPlain($folders),
+            'files' => $this->formatIteratorPlain($files),
+        ];
+
+        return $this->json($data);
+    }
+}
