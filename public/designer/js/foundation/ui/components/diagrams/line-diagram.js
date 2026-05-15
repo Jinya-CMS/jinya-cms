3a811752a05d3f3dd324b41560ca6daccbf6d882
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2026-05-16 00:21:38 +0200

    JCMS-33: Added new line chart

--- public/designer/js/foundation/ui/components/diagrams/line-diagram.js
+++ public/designer/js/foundation/ui/components/diagrams/line-diagram.js
@@ -1,106 +1,41 @@
+import { DiagramBase } from './diagram-base.js';
 import localize from '../../../utils/localize.js';
-import { BaseDiagram } from './base-diagram.js';
 
-class LineDiagramElement extends BaseDiagram {
+class LineDiagramElement extends DiagramBase {
   constructor() {
-    super('line', null);
+    super();
+  }
+
+  static get observedAttributes() {
+    return DiagramBase.observedAttributes;
   }
 
   connectedCallback() {
     super.connectedCallback();
   }
 
-  static get observedAttributes() {
-    return BaseDiagram.observedAttributes;
-  }
-
   attributeChangedCallback(property, oldValue, newValue) {
     return super.attributeChangedCallback(property, oldValue, newValue);
   }
 
-  get interval() {
-    return this.getAttribute('interval');
-  }
-
-  set interval(value) {
-    this.setAttribute('interval', value);
-    this.renderDiagram();
-  }
-
-  get name() {
-    return localize({ key: `statistics.access.visits` });
-  }
-
   getOptions(stats) {
     return {
-      series: [
-        {
-          name: this.name,
-          data: stats.map((item) => ({
-            y: item.visits.toLocaleString(),
-            x: item.group,
-          })),
-        },
-      ],
-      chart: {
-        type: 'area',
-        stacked: false,
-        height: 360,
-        toolbar: {
-          show: false,
-        },
-        zoom: {
-          enabled: false,
-        },
-        style: BaseDiagram.chartStyle,
-      },
-      dataLabels: {
-        enabled: false,
-      },
-      markers: {
-        size: 0,
-      },
-      title: {
-        text: localize({ key: `statistics.access.${this.group.replace('-', '_')}.${this.interval.replace('-', '_')}` }),
-        align: 'left',
-        style: BaseDiagram.titleStyle,
-      },
-      theme: {
-        monochrome: BaseDiagram.theme,
-      },
-      fill: {
-        type: 'gradient',
-        gradient: {
-          shadeIntensity: 1,
-          inverseColors: false,
-          opacityFrom: 0.5,
-          opacityTo: 0,
-          stops: [0, 90, 100],
-        },
-      },
-      yaxis: {
-        labels: {
-          formatter(item) {
-            return item.toLocaleString();
-          },
-        },
-        style: BaseDiagram.axisStyle,
-      },
-      xaxis: {
-        type: 'datetime',
-        labels: {
-          style: BaseDiagram.axisStyle,
-        },
-      },
-      tooltip: {
-        shared: false,
-        style: BaseDiagram.tooltipStyle,
-        y: {
-          formatter(item) {
-            return item.toLocaleString();
-          },
-        },
-      },
+      ...super.getOptions(stats),
+      xAxis: [{
+        type: 'category',
+        boundaryGap: false,
+        data: stats.map((s) => new Date(Date.parse(s.group)).toLocaleDateString()),
+      }],
+      yAxis: [{
+        type: 'value',
+      }],
+      series: [{
+        showSymbol: false,
+        type: 'line',
+        smooth: true,
+        data: stats.map((s) => s.visits),
+        name: localize({ key: `statistics.access.visits` }),
+      }],
     };
   }
 }
--- public/designer/js/frontstage/statistics/:interval.js
+++ public/designer/js/frontstage/statistics/:interval.js
@@ -1,7 +1,7 @@
-import '../../foundation/ui/components/diagrams/treemap-diagram.js';
-import '../../foundation/ui/components/diagrams/bar-diagram.js';
+import '../../foundation/ui/components/diagrams.bak/treemap-diagram.js';
+import '../../foundation/ui/components/diagrams.bak/bar-diagram.js';
 import '../../foundation/ui/components/diagrams/line-diagram.js';
-import '../../foundation/ui/components/diagrams/pie-diagram.js';
+import '../../foundation/ui/components/diagrams.bak/pie-diagram.js';
 import { Alpine } from '../../../../lib/alpine.js';
 import localize from '../../foundation/utils/localize.js';
 import { getTotalVisitsForInterval } from '../../foundation/api/statistics.js';
