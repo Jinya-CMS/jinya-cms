b03d5418c5d81895fcc157d5841046cecad62b27
Imanuel Ulbricht <imanuel.ulbricht@jinya.de>
2026-05-22 03:22:51 +0200

    Chore: Fix an issue with api docs link

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -104,7 +104,7 @@ const config: Config = {
                         },
                         {
                             label: 'API Documentation',
-                            to: '/docs/api-docs',
+                            to: '/docs/api/jinya-cms-api',
                         },
                     ],
                 },
